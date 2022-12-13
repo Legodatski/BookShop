dc59a5c37ceb6508f45247f2ff93c363de498a0b
Legodatski <ovinedev@abv.bg>
2022-12-13 18:10:04 +0200

    updated appsettings

--- BookShop/Controllers/UsersController.cs
+++ BookShop/Controllers/UsersController.cs
@@ -76,7 +76,7 @@ namespace BookShop.Controllers
             if (result.Succeeded)
             {
                 await userManager.AddToRoleAsync(user, "User");
-                return RedirectToAction("Login", "Account");
+                return RedirectToAction("Login", "Users");
             }
 
             foreach (var item in result.Errors)
